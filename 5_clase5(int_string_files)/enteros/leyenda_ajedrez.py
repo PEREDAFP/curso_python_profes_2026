9dff35ce700782bee278ac9f565159f7fa923529
pedro <pedropereda@hotmail.com>
2026-02-18 12:47:32 +0100

    Modificamos tablero de ajedrez.py

--- 5_clase5(int_string_files)/enteros/leyenda_ajedrez.py
+++ 5_clase5(int_string_files)/enteros/leyenda_ajedrez.py
@@ -1,18 +1,12 @@
 #!/usr/bin/env python3
 # Actividad A: El crecimiento exponencial en un tablero de ajedrez
 casillas = 64
-granos = 1  # Empezamos con 1 grano
 total_granos = 0
-
-print(f"Casilla 1: {granos} grano")
-
-for i in range(2, casillas + 1):
-    granos = granos * 2
-    total_granos += granos
-    # Imprimimos solo algunas para no saturar la pantalla
-    if i == 64:
-        print(f"Casilla {i}: {granos} granos")
-
+for i in range(0, casillas):
+   
+    total_granos += 2**i
+    print(f"Para la casilla {i}: total granos:{total_granos} en esta casilla:{2**i}")
+    
 # Reto visual: Python permite usar guiones bajos para leer mejor los números
 print("\nTotal legible:")
 print(f"{total_granos:,}".replace(",", ".")) # Formato europeo
